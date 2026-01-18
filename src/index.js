921a4566b8bc8896ad71f81559b27da1f14cb809
iTzMeYuGBoo <yugandharreddybana@outlook.com>
2026-01-18 02:19:26 +0000

    Restore GitHub Pages base path

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import './styles/index.css'
 import { AuthProvider } from './context/AuthContext.js'
 import { ThemeProvider } from './context/ThemeContext.js'
 
+const basename = process.env.NODE_ENV === 'production' ? '/Portfolio' : '/'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HelmetProvider>
       <ThemeProvider>
         <AuthProvider>
-          <Router>
+          <Router basename={basename}>
             <App />
           </Router>
         </AuthProvider>
